3829244b7f0188c35986174af92b88fdf070dba9
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-08 10:02:07 +0300

    Add Entity abstract class

--- companydb/src/company/Customers.java
+++ companydb/src/company/Customers.java
@@ -1,1 +1,40 @@
 package company;
+
+public class Customers extends Entity {
+    private String name;
+    private String address;
+    private Integer total_pnumber;
+    private Integer complet_pnumber;
+        
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public Integer getTotal_pnumber() {
+        return total_pnumber;
+    }
+    
+    public void setTotal_pnumber(Integer number) {
+        this.total_pnumber = number;
+    }
+    
+    public Integer getComplet_pnumbe() {
+        return complet_pnumber;
+    }
+    
+    public void setComplet_pnumbe(Integer number) {
+        this.complet_pnumber = number;
+    }
+}
