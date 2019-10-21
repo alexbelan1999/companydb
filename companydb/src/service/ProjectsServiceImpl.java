66c21dc67673074672985a2319957f4954a47ca2
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-21 17:20:05 +0300

    Add ProjectsServiceImpl class

---
+++ companydb/src/service/ProjectsServiceImpl.java
@@ -0,0 +1,128 @@
+package service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dao.CustomersDao;
+import dao.ManagersDao;
+import dao.ProjectsDao;
+import company.Customers;
+import company.Managers;
+import company.Projects;
+
+public class ProjectsServiceImpl implements ProjectsService {
+    private CustomersDao customerDao;
+    private ManagersDao managerDao;
+    private ProjectsDao projectDao;
+
+    @Override
+    public List<Projects> findByProject(Long project_id) {
+        List<Projects> projects = projectDao.readByProject(project_id);
+        Map<Long, Customers> customers = new HashMap<>();
+        Map<Long, Managers> managers = new HashMap<>();
+        for(Projects project : projects) {
+            Customers customer = project.getCustomer();
+            Managers manager = project.getManager();
+
+            if(customer != null) {
+                Long id = customer.getId();
+                customer = customers.get(id);
+                if(customer == null) {
+                    customer = customerDao.read(id);
+                    customers.put(id, customer);
+                }
+                project.setCustomer(customer);
+            }
+
+            if(manager != null) {
+                Long id = manager.getId();
+                manager = managers.get(id);
+                if(manager == null) {
+                    manager = managerDao.read(id);
+                    managers.put(id, manager);
+                }
+                project.setManager(manager);
+            }
+        }
+        return projects;
+    }
+    
+    public List<Projects> findByCustomer(Long customer_id) {
+        List<Projects> projects = projectDao.readByCustomer(customer_id);
+        Map<Long, Customers> customers = new HashMap<>();
+        Map<Long, Managers> managers = new HashMap<>();
+        for(Projects project : projects) {
+            Customers customer = project.getCustomer();
+            Managers manager = project.getManager();
+
+            if(customer != null) {
+                Long id = customer.getId();
+                customer = customers.get(id);
+                if(customer == null) {
+                    customer = customerDao.read(id);
+                    customers.put(id, customer);
+                }
+                project.setCustomer(customer);
+            }
+
+            if(manager != null) {
+                Long id = manager.getId();
+                manager = managers.get(id);
+                if(manager == null) {
+                    manager = managerDao.read(id);
+                    managers.put(id, manager);
+                }
+                project.setManager(manager);
+            }
+        }
+        return projects;
+    }
+    
+    public List<Projects> findByManager(Long manager_id) {
+        List<Projects> projects = projectDao.readByManager(manager_id);
+        Map<Long, Customers> customers = new HashMap<>();
+        Map<Long, Managers> managers = new HashMap<>();
+        for(Projects project : projects) {
+            Customers customer = project.getCustomer();
+            Managers manager = project.getManager();
+
+            if(customer != null) {
+                Long id = customer.getId();
+                customer = customers.get(id);
+                if(customer == null) {
+                    customer = customerDao.read(id);
+                    customers.put(id, customer);
+                }
+                project.setCustomer(customer);
+            }
+
+            if(manager != null) {
+                Long id = manager.getId();
+                manager = managers.get(id);
+                if(manager == null) {
+                    manager = managerDao.read(id);
+                    managers.put(id, manager);
+                }
+                project.setManager(manager);
+            }
+        }
+        return projects;
+    }
+    
+    public List<Projects> findAll() {
+        return projectDao.readAll();
+    }
+    
+    public void setCustomersDao(CustomersDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public void setManagersDao(ManagersDao managerDao) {
+        this.managerDao = managerDao;
+    }
+    
+    public void setProjectsDao(ProjectsDao projectDao) {
+        this.projectDao = projectDao;
+    }
+}
