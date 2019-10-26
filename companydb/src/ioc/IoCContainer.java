5390300370d66c83175bdb34290fe9bb71886b2f
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-26 16:36:02 +0300

    Change IoCContainer class

--- companydb/src/ioc/IoCContainer.java
+++ companydb/src/ioc/IoCContainer.java
@@ -1,88 +1,70 @@
 package ioc;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class IoCContainer implements AutoCloseable {
-    private static Map<Class<?>, Class<?>> dependencyInversionMap = new HashMap<>();
-    private static Map<Class<?>, Map<Class<?>, Method>> dependencyInjectionMap = new HashMap<>();
-    private static Map<Class<?>, Factory<?>> factories = new HashMap<>();
+	private static Map<Class<?>, Class<?>> dependencyInversionMap = new HashMap<>();
+	private static Map<Class<?>, Factory<?>> factories = new HashMap<>();
 
-    private Map<Class<?>, Object> cache = new HashMap<>();
+	private Map<Class<?>, Object> cache = new HashMap<>();
+	private DIContainer dic = new DIContainer(this);
 
-    @SuppressWarnings("unchecked")
-    public <T> T get(Class<T> key) throws IoCException {
-        T object = (T)cache.get(key);
-        if(object == null) {
-            try {
-                Class<?> value = dependencyInversionMap.get(key);
-                if(value != null) {
-                    object = (T)value.newInstance();
-                    cache.put(key, object);
-                    Map<Class<?>, Method> dependencies = dependencyInjectionMap.get(value);
-                    if(dependencies != null) {
-                        for(Map.Entry<Class<?>, Method> entry : dependencies.entrySet()) {
-                            Class<?> dependency = entry.getKey();
-                            Method injector = entry.getValue();
-                            injector.invoke(object, get(dependency));
-                        }
-                    }
-                } else {
-                    Factory<?> factory = factories.get(key);
-                    if(factory != null) {
-                        object = (T)factory.get();
-                        cache.put(key, object);
-                    }
-                }
-            } catch(InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                throw new IoCException(e);
-            }
-        }
-        return object;
-    }
+	public <T> T get(Class<T> key) throws IoCException {
+		return get(key, null);
+	}
 
-    public static void registerClass(String abstraction, String implemetation) throws IoCException {
-        registerClass(abstraction, implemetation, null);
-    }
+	@SuppressWarnings("unchecked")
+	public <T> T get(Class<T> key, String additionalKey) throws IoCException {
+		T object = (T)cache.get(key);
+		if(object == null) {
+			try {
+				Class<?> value = dependencyInversionMap.get(key);
+				if(value != null) {
+					object = (T)value.newInstance();
+				} else {
+					Factory<?> factory = factories.get(key);
+					if(factory != null) {
+						object = (T)factory.get(additionalKey);
+					}
+				}
+				if(object != null) {
+					cache.put(key, object);
+					dic.injectDependencies(object);
+				}
+			} catch(InstantiationException | IllegalAccessException e) {
+				throw new IoCException(e);
+			}
+		}
+		return object;
+	}
 
-    public static void registerClass(String abstraction, String implemetation, Map<String, String> dependencies) throws IoCException {
-        try {
-            Class<?> actualAbstraction = Class.forName(abstraction);
-            Class<?> actualImplemetation = Class.forName(implemetation);
-            dependencyInversionMap.put(actualAbstraction, actualImplemetation);
-            if(dependencies != null) {
-                Map<Class<?>, Method> actualDependencies = new HashMap<>();
-                dependencyInjectionMap.put(actualImplemetation, actualDependencies);
-                for(Map.Entry<String, String> entry : dependencies.entrySet()) {
-                    Class<?> dependency = Class.forName(entry.getKey());
-                    String injectorName = entry.getValue();
-                    Method injector = actualImplemetation.getMethod(injectorName, dependency);
-                    actualDependencies.put(dependency, injector);
-                }
-            }
-        } catch(ClassNotFoundException | NoSuchMethodException e) {
-            throw new IoCException(e);
-        }
-    }
+	public static void registerClass(String abstraction, String implemetation) throws IoCException {
+		try {
+			Class<?> actualImplemetation = Class.forName(implemetation);
+			Class<?> actualAbstraction = Class.forName(abstraction);
+			dependencyInversionMap.put(actualAbstraction, actualImplemetation);
+		} catch(ClassNotFoundException e) {
+			throw new IoCException(e);
+		}
+	}
 
-    public static void registerFactory(String abstraction, String factory) throws IoCException {
-        try {
-            Class<?> actualAbstraction = Class.forName(abstraction);
-            Class<?> actualFactory = Class.forName(factory);
-            factories.put(actualAbstraction, (Factory<?>)actualFactory.newInstance());
-        } catch(ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            throw new IoCException(e);
-        }
-    }
+	public static void registerFactory(String abstraction, String factory) throws IoCException {
+		try {
+			Class<?> actualAbstraction = Class.forName(abstraction);
+			Class<?> actualFactory = Class.forName(factory);
+			factories.put(actualAbstraction, (Factory<?>)actualFactory.newInstance());
+		} catch(ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new IoCException(e);
+		}
+	}
 
-    @Override
-    public void close() {
-        for(Object object : cache.values()) {
-            if(object instanceof AutoCloseable) {
-                try { ((AutoCloseable)object).close(); } catch(Exception e) {}
-            }
-        }
-    }
+	@Override
+	public void close() {
+		for(Object object : cache.values()) {
+			if(object instanceof AutoCloseable) {
+				try { ((AutoCloseable)object).close(); } catch(Exception e) {}
+			}
+		}
+	}
 }
