4775071b6d56ed1c0a2e306838a84cbd5d7df971
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-26 17:07:45 +0300

    Change CustomersServiceImpl class

--- companydb/src/service/CustomersServiceImpl.java
+++ companydb/src/service/CustomersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import dao.CustomersDao;
 import dao.DaoException;
 import company.Customers;
+import company.Managers;
 
 public class CustomersServiceImpl implements CustomersService {
     private CustomersDao customerDao;
@@ -13,6 +14,37 @@ public class CustomersServiceImpl implements CustomersService {
     public List<Customers> findAll() throws ServiceException {
         try {
             return customerDao.readAll();
+        } catch(DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+    
+    @Override
+    public Customers findById(Long id) throws ServiceException {
+        try {
+            return customerDao.read(id);
+        } catch(DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public void save(Customers customer) throws ServiceException {
+        try {
+            if(customer.getId() != null) {
+                customerDao.update(customer);
+            } else {
+                customerDao.create(customer);
+            }
+        } catch(DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public void delete(Long id) throws ServiceException {
+        try {
+            customerDao.delete(id);
         } catch(DaoException e) {
             throw new ServiceException(e);
         }
