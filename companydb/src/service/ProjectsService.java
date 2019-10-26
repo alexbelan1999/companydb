02e346e756176b6180760113ed6a0ae2ccb4cb0c
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-26 17:10:15 +0300

    Change ProjectsService interface

--- companydb/src/service/ProjectsService.java
+++ companydb/src/service/ProjectsService.java
@@ -1,10 +1,14 @@
 package service;
 
 import java.util.List;
+
 import company.Projects;
 
 public interface ProjectsService {
     List<Projects> findByCustomer(Long customer_id) throws ServiceException;
     List<Projects> findByManager(Long manager_id) throws ServiceException;
     List<Projects> findAll() throws ServiceException;
+    Projects findById(Long id) throws ServiceException;
+    void save(Projects project) throws ServiceException;
+    void delete(Long id) throws ServiceException;
 }
