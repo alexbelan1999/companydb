a10ce55d8ec778cf0080da57591795bf337bf810
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-26 16:56:56 +0300

    Change ManagersServiceImpl class

--- companydb/src/service/ManagersServiceImpl.java
+++ companydb/src/service/ManagersServiceImpl.java
@@ -17,7 +17,38 @@ public class ManagersServiceImpl implements ManagersService {
             throw new ServiceException(e);
         }
     }
+    
+    @Override
+    public Managers findById(Long id) throws ServiceException {
+        try {
+            return managerDao.read(id);
+        } catch(DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
 
+    @Override
+    public void save(Managers manager) throws ServiceException {
+        try {
+            if(manager.getId() != null) {
+                managerDao.update(manager);
+            } else {
+                managerDao.create(manager);
+            }
+        } catch(DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public void delete(Long id) throws ServiceException {
+        try {
+            managerDao.delete(id);
+        } catch(DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+    
     public void setManagersDao(ManagersDao managerDao) {
         this.managerDao = managerDao;
     }
