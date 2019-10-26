cc418c6718c20fe3a7b3878c9951744e4056cbbb
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-26 16:47:13 +0300

    Change ConnectionFactory class

--- companydb/src/pool/ConnectionFactory.java
+++ companydb/src/pool/ConnectionFactory.java
@@ -6,12 +6,12 @@ import ioc.Factory;
 import ioc.IoCException;
 
 public class ConnectionFactory implements Factory<Connection> {
-    @Override
-    public Connection get() throws IoCException {
-        try {
-            return ConnectionPool.getInstance().getConnection();
-        } catch(PoolException e) {
-            throw new IoCException(e);
-        }
-    }
+	@Override
+	public Connection get(String key) throws IoCException {
+		try {
+			return ConnectionPool.getInstance().getConnection();
+		} catch(PoolException e) {
+			throw new IoCException(e);
+		}
+	}
 }
