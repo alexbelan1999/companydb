aa76b38860d3a93dab98dced358bf19a06690a0f
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-26 16:51:45 +0300

    Change ManagersService interface

--- companydb/src/service/ManagersService.java
+++ companydb/src/service/ManagersService.java
@@ -6,4 +6,7 @@ import company.Managers;
 
 public interface ManagersService {
     List<Managers> findAll() throws ServiceException;
+    Managers findById(Long id) throws ServiceException;
+    void save(Managers manager) throws ServiceException;
+    void delete(Long id) throws ServiceException;
 }
