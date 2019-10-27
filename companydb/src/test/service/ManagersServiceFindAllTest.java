5ccc4d134ff8840e972c499649a08d99a0d2833e
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-27 10:52:55 +0300

    Change ManagersServiceFindAllTest class

--- companydb/src/test/service/ManagersServiceFindAllTest.java
+++ companydb/src/test/service/ManagersServiceFindAllTest.java
@@ -16,14 +16,16 @@ public class ManagersServiceFindAllTest {
     public static void main(String[] args) throws IoCException, ServiceException, PoolException {
         ConnectionPool.getInstance().init("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:3306/companydb?useUnicode=true&characterEncoding=UTF8&useSSL=false&allowPublicKeyRetrieval=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "1234");
         IoCConfigurer.configure();
-        IoCContainer ioc = new IoCContainer();
-        ManagersService managerService = ioc.get(ManagersService.class);
-        List<Managers> managers = managerService.findAll();
-        System.out.println("Список всех менеджеров");
-        System.out.println("===================");
-        for(Managers manager : managers) {
-            System.out.println(Utility.toString(manager));
+        try(IoCContainer ioc = new IoCContainer()) {
+            ManagersService managerService = ioc.get(ManagersService.class);
+            List<Managers> managers = managerService.findAll();
+            System.out.println("Список всех менеджеров");
+            System.out.println("===================");
+            for(Managers manager : managers) {
+                System.out.println(Utility.toString(manager));
+            }
+        } finally {
+            ConnectionPool.getInstance().destroy();
         }
-        ConnectionPool.getInstance().destroy();
     }
 }
